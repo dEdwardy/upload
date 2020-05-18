37904edf5b41b6baa36bb02630fde5272132f583
luohaiming <v-luohaiming@geekplus.com.cn>
2020-05-18 18:11:33 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import element from 'element-ui'
 Vue.config.productionTip = false
-
+Vue.use(element)
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="file" @change="handleFileChange" />
+    <el-button @click="handleUpload">Upload</el-button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+const SIZE = 10 * 1024 * 1024; // 切片大小
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      container: {
+        file: null,
+      },
+      data: [],
+    };
+  },
+  methods: {
+    request({ url, method = "post", data, headers = {}, requestList }) {
+      return new Promise((resolve) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        Object.keys(headers).forEach((key) =>
+          xhr.setRequestHeader(key, headers[key])
+        );
+        xhr.send(data);
+        xhr.onload = (e) => {
+          resolve({
+            data: e.target.response,
+          });
+        };
+      });
+    },
+    handleFileChange(e) {
+      const [file] = e.target.files;
+      if (!file) return;
+      this.container.file = file;
+      console.log(this.container.file);
+    },
+    //生成切片文件
+    createFileChunk(file, size = SIZE) {
+      const fileChunkList = [];
+      let cur = 0;
+      while (file.size > cur) {
+        fileChunkList.push({ file: file.slice(cur, cur + size) });
+        cur += size;
+      }
+      return fileChunkList;
+    },
+    //上传切片文件
+    async uploadChunks() {
+      const requestList = this.data
+        .map(({ chunk, hash }) => {
+          const formData = new FormData();
+          formData.append("chunk", chunk);
+          formData.append("hash", hash);
+          formData.append("filename", this.container.file.name);
+          return { formData };
+        })
+        .map(async ({ formData }) => {
+          this.request({
+            url: "http://localhost:3000/post",
+            data: formData,
+          });
+        });
+      await Promise.all(requestList); //并发切片
+
+      await this.mergeRequest(); // 合并切片
+    },
+    async handleUpload() {
+      if (!this.container.file) return;
+      const fileChunkList = this.createFileChunk(this.container.file);
+      this.data = fileChunkList.map(({ file }, index) => ({
+        chunk: file,
+        hash: this.container.file.name + "-" + index,
+      }));
+      await this.uploadChunks();
+    },
+    async mergeRequest() {
+      await this.request({
+        url: "http://localhost:3000/merge",
+        headers: { "content-type": "application/json" },
+        data: JSON.stringify({
+          filename: this.container.file.name,
+          size: SIZE,
+        }),
+      });
+    },
+  },
+};
 </script>
