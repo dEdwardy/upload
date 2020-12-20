665920f9e2d02e3f5115e818f0a9ac0faa60a325
edw4rd6731 <957196958@qq.com>
2020-12-20 23:06:18 +0800

    upload slice

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,16 +7,23 @@
 </template>
 
 <script>
-import { encode } from '@/utils'
-import axios from 'axios'
-// const SIZE = 10 * 1024 * 1024; // 切片大小
-const SIZE =  1024; // 切片大小
+//:TODO  PROMISE-LIMIT
+import { encode } from "@/utils";
+import axios from "axios";
+import { read } from "fs";
+import promiseLimit from "promise-limit";
+// import async from "async";
+const async = require("async");
+const SIZE = 10 * 1024 * 1024; // 切片大小
+// const SIZE =  1024; // 切片大小
 export default {
   name: "Home",
   data() {
     return {
-      file:null,
-      data: []
+      file: null,
+      data: [],
+      hash: "",
+      tasks: []
     };
   },
   methods: {
@@ -46,7 +53,10 @@ export default {
       const fileChunkList = [];
       let cur = 0;
       while (file.size > cur) {
-        fileChunkList.push({ file: file.slice(cur, cur + size) });
+        fileChunkList.push({
+          file: file.slice(cur, cur + size),
+          cur
+        });
         cur += size;
       }
       return fileChunkList;
@@ -54,47 +64,120 @@ export default {
     //上传切片文件
     async uploadChunks() {
       const requestList = this.data
-        .map(({ chunk, hash }) => {
-          const formData = new FormData();
-          formData.append("chunk", chunk);
-          formData.append("hash", hash);
+        .map(({ chunk, hash, total, index }) => {
+          const formData = new FormData(); 
+          let reader = new FileReader()
+          read.on()
+          formData.append("file", chunk);
+          // formData.append("hash", hash);
           formData.append("filename", this.file.name);
+          formData.append("current", index);
+          formData.append("total", total);
           return { formData };
         })
-        .map(async ({ formData }) => {
-          axios.post('http://localhost:3000/files/v2',data)
+        .map(({ formData }, idx) => {
+          return { formData, idx };
+          // return async () =>  {
+          //   console.warn(idx)
+          //   return await axios.post(
+          //   `http://localhost:3000/files/p1/${this.file.name}-${idx}`,
+          //   formData
+          // ).then(res => console.log(res)).catch(e => console.warn(e))
+          // }
         });
-      await Promise.all(requestList); //并发切片
+      let total = requestList.length;
+      let filename = this.file.name;
+      console.log(requestList);
+      requestList.forEach(async ({ formData, idx }) => { 
+        console.log(idx)
+         await axios
+          .post(
+            `http://localhost:3000/files/p1/${this.file.name}-${idx}`,
+            formData
+          )
+      });
+      // let q = async.queue(( cb) => {
+      //   console.log(cb)
+      //   cb();
+      // }, 6);
+      // q.drain(() => {
+      //   console.log("切片上传完毕");
+      //   return axios.post("http://localhost:3000/files/p1-merge", {
+      //     filename,
+      //     total
+      //   });
+      // });
+      // requestList.map( i=> q.push(i))
 
-      await this.mergeRequest(); // 合并切片
+      // Promise.all(requestList)
+      //   .then(res => {
+      //     // console.log(res)
+      //     let filename = this.file.name;
+      //     return axios.post("http://localhost:3000/files/p1-merge", {
+      //       filename,
+      //       total
+      //     });
+      //     console.timeEnd("promise");
+      //     //全部上传完毕
+      //     // axios.post("http://localhost:3000/files/p1/"+)
+      //   })
+      //   .catch(err => {
+      //     //至少有一个切片上传失败
+      //     console.warn(err);
+      //   });
+
+      // await this.mergeRequest(); // 合并切片
     },
-    handleUpload2(){
+    async handleUpload2() {
       if (!this.file) return;
       let data = new FormData();
-      let hash = encode(JSON.stringify(this.file))
-      data.append('file',this.file)
-      data.append('hash',hash)
-      return axios.post('http://localhost:3000/files/v2',data)
+      // this.hash = encode(JSON.stringify(this.file));
+      let reader = new FileReader();
+      reader.readAsDataURL(this.file);
+      reader.onload = async () => {
+        console.warn(this);
+        this.hash = reader.result;
+        const fileChunkList = this.createFileChunk(this.file);
+        this.data = fileChunkList.map(({ file }, index) => ({
+          chunk: file,
+          hash: this.hash + "-" + index,
+          total: fileChunkList.length,
+          index
+        }));
+        await this.uploadChunks();
+      };
+
+      // data.append('file',this.file)
+      // data.append('hash',hash)
+      // let filename =  this.file.name
+      // return axios.post('http://localhost:3000/files/p1/merge',{
+      //   filename
+      // })
     },
     async handleUpload() {
       if (!this.file) return;
-      let hash = encode(JSON.stringify(this.file))
+      let hash = encode(JSON.stringify(this.file));
       const fileChunkList = this.createFileChunk(this.file);
-      this.data = fileChunkList.map(({ file }, index) => ({
+      this.data = fileChunkList.map(({ file }, index, end) => ({
         chunk: file,
-        hash: hash+ "-" + index
+        hash: hash + "-" + index,
+        end
       }));
       await this.uploadChunks();
     },
-    async mergeRequest() {
-      await this.request({
-        url: "http://localhost:3000/merge",
-        headers: { "content-type": "application/json" },
-        data: JSON.stringify({
-          filename: this.file.name,
-          size: SIZE
-        })
+    mergeRequest() {
+      let hash = this.hash;
+      return axios.post("http://localhost:3000/files/merge", {
+        hash
       });
+      // await this.request({
+      //   url: "http://localhost:3000/merge",
+      //   headers: { "content-type": "application/json" },
+      //   data: JSON.stringify({
+      //     filename: this.file.name,
+      //     size: SIZE
+      //   })
+      // });
     }
   }
 };
