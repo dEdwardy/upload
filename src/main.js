eebbf18d69b7ae0c17d8646a408f15ea991e9acf
edw4rd <957196958@qq.com>
2021-01-13 17:35:11 +0800

    add echarts

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
-/*
- * @Author: Edw4rd
- * @Date: 2020-12-18 13:47:20
- * @LastEditTime: 2020-12-25 11:28:35
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \upload\src\main.js
- */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import element from 'element-ui'
+import VCharts from 'v-charts'
 Vue.config.productionTip = false
 Vue.use(element)
+Vue.use(VCharts)
 new Vue({
   router,
   store,
---
+++ src/views/Visualize.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="visual">
+    <div style="height: 300px">
+      <ve-heatmap
+        :data="chartData"
+        :visual-map="visualMap"
+        :settings="chartSettings"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "visual",
+  data() {
+    this.chartSettings = {
+      position: "china",
+      type: "map",
+      geo: {
+        label: {
+          emphasis: {
+            show: true
+          }
+        }
+        //   itemStyle: {
+        //     normal: {
+        //       areaColor: '#323c48',
+        //       borderColor: '#111'
+        //     },
+        //     emphasis: {
+        //       areaColor: '#2a333d'
+        //     }
+        //   }
+      }
+    };
+    this.visualMap = {
+      type: "piecewise",
+      min: 200,
+      max: 1000,
+      realtime: false,
+      calculable: true,
+      inRange: {
+        color: [
+          "#fee090",
+          "#fdae61",
+          "#f46d43",
+          "#d73027",
+          "#a50026"
+        ]
+      }
+    };
+    return {
+      chartData: {
+        columns: ["lat", "lng", "人数"],
+        rows: [
+          { lat: 115.892151, lng: 28.676493, 人数: 1000 },
+          { lat: 117.000923, lng: 36.675807, 人数: 400 },
+          { lat: 113.665412, lng: 34.757975, 人数: 800 },
+          { lat: 114.298572, lng: 30.584355, 人数: 200 },
+          { lat: 112.982279, lng: 28.19409, 人数: 100 },
+          { lat: 113.280637, lng: 23.125178, 人数: 300 },
+          { lat: 110.33119, lng: 20.031971, 人数: 800 },
+          { lat: 104.065735, lng: 30.659462, 人数: 700 },
+          { lat: 108.948024, lng: 34.263161, 人数: 300 },
+          { lat: 103.823557, lng: 36.058039, 人数: 500 }
+        ]
+      }
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.visual {
+}
+</style>
